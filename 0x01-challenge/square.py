fda3511304e25196fdbe0666120e3ec0c9fa9184
abdou50 <gadourraddadi@gmail.com>
2022-10-03 00:01:00 +0300

    l

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""oopppppppppppppppppppppppppppppppppppppp"""
+""" Define class Square"""
 
 
-class square():
-    """oppppppppppppppppppppppppppppppppppp"""
+class Square():
+    """A class of Square that calculate the square """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """oopppppppppppppppppppppppppppppppppppppppppppppppppp pppp"""
+        """ initialise a Square class key value """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,18 +16,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ooppppppppppppppppppppppppppppppppppppppppp oop"""
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """oopppppppppppppppppppppppppppppppppppppppppppppppppppppppppp oop"""
+        """ Repsontation avec str  the square by defined string """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
